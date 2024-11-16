7ee8b0193e3a116eda988b928f1f025c0c588bb9
jsoulier <jaansoulier@gmail.com>
2024-11-16 16:44:11 -0500

    fixed ui rendering bug

--- shaders/helpers.glsl
+++ shaders/helpers.glsl
@@ -72,14 +72,19 @@ bool get_shadow(const uint voxel)
     return bool(voxel >> VOXEL_SHADOW_OFFSET & VOXEL_SHADOW_MASK);
 }
 
+bool get_shadowed(const uint voxel)
+{
+    return bool(voxel >> VOXEL_SHADOWED_OFFSET & VOXEL_SHADOWED_MASK);
+}
+
 vec3 get_sky(const float y)
 {
-    return mix(vec3(0.3, 0.6, 0.9), vec3(0.8, 0.95, 1.0), max(y - 0.5, 0.0));
+    return mix(vec3(0.3, 0.6, 0.9), vec3(0.8, 0.95, 1.0), max(y - 0.6, 0.0));
 }
 
 float get_fog(const vec2 position, const vec2 camera)
 {
-    return pow(clamp(length(position - camera) / 250.0, 0.0, 1.0), 2.5);
+    return min(pow(length(position - camera) / 250.0, 2.5), 1.0);
 }
 
 float get_random(const vec2 position)
@@ -87,69 +92,26 @@ float get_random(const vec2 position)
     return fract(sin(dot(position, vec2(12.9898, 78.233))) * 43758.5453);
 }
 
-float get_light(const vec3 normal, const vec3 camera)
-{
-    return max(dot(normal, -camera), 0.0);
-}
-
-bool get_shadowed(
-    const vec3 normal,
-    const vec3 camera,
-    const vec3 position,
-    const sampler2D map)
-{
-    return
-        (dot(normal, -camera) < 0.0) || (
-        all(greaterThanEqual(position, vec3(0.0))) &&
-        all(lessThanEqual(position, vec3(1.0))) &&
-        (position.z - 0.0005 > texture(map, position.xy).x));
-}
-
-bool get_edge(
-    const uint direction,
-    const vec3 position,
-    const vec3 neighbor)
-{
-    switch (direction)
-    {
-    case 4: return position.y < neighbor.y;
-    case 5: return position.y > neighbor.y;
-    case 2: return position.x > neighbor.x;
-    case 3: return position.x < neighbor.x;
-    case 0: return position.z < neighbor.z;
-    case 1: return position.z > neighbor.z;
-    }
-    return false;
-}
-
 vec4 get_color(
-    const vec4 color,
+    const sampler2D atlas,
+    const sampler2D shadowmap,
+    const vec2 uv,
+    const vec3 shadow_position,
+    const vec3 shadow_vector,
     const bool shadowed,
-    const float ssao,
-    const float fog,
     const vec3 normal,
-    const vec3 camera)
+    const float fog,
+    const float ssao)
 {
     float a;
     float b;
     float c;
-    const float light = get_light(normal, camera);
-#if DRAW_NORMALS
-    return vec4(vec3(normal * 0.5 + 0.5), 1.0);
-#endif
-#if DRAW_SSAO
-    return vec4(vec3(ssao), 1.0);
-#endif
-#if DRAW_LIGHTS
-    return vec4(vec3(light), 1.0);
-#endif
-#if DRAW_COLORS
-    return vec4(color);
-#endif
-#if DRAW_SHADOWED
-    return vec4(vec3(!shadowed), 1.0);
-#endif
-    if (shadowed)
+    const float angle = dot(normal, -shadow_vector);
+    const float depth = shadow_position.z - 0.001;
+    if (shadowed && ((angle < 0.0) || (
+        all(greaterThanEqual(shadow_position, vec3(0.0))) &&
+        all(lessThanEqual(shadow_position, vec3(1.0))) &&
+        (depth > texture(shadowmap, shadow_position.xy).x))))
     {
         a = ssao * 0.2;
         b = 0.0;
@@ -159,8 +121,9 @@ vec4 get_color(
     {
         a = ssao * 0.3;
         b = 0.4;
-        c = light * 0.6;
+        c = max(angle, 0.0) * 0.6;
     }
+    const vec4 color = texture(atlas, uv);
     const vec4 composite = vec4(color.xyz * (a + b + c + 0.3), color.a);
     const vec4 sky = vec4(get_sky(0.0), 1.0);
     return mix(composite, sky, fog);
